23ecc23c28921074017d6d45e0496f37a9ef96f1
Emily Vinsen <emilyv@flocc.co>
2022-08-19 10:36:31 +0100

    updated to follow nuxt docs instead of module builder
    https://nuxtjs.org/docs/configuration-glossary/configuration-components/#library-authors

---
+++ components/List.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <p>List here: {{ posts }}</p>
+    <h2>For loop below:</h2>
+    <div
+      v-for="item in posts"
+      :key="item.id"
+    >
+      <p>{{ item.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      default: null
+    }
+  }
+}
+</script>
