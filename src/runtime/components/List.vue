569e3acffb5de9868e598b8f2ae0c69f76ec45cd
Emily Vinsen <emilyv@flocc.co>
2022-08-19 10:14:44 +0100

    first commit

---
+++ src/runtime/components/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-r24/24 mx-auto">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-medium">
+      <div
+        v-for="item in posts"
+        :key="item.id"
+      >
+        <p>{{ item.title }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      default: null
+    },
+    dates: {
+      type: Object,
+      default: null
+    },
+    canBook: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
